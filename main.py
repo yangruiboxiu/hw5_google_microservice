c0620efc6a10751a84856f4aca7afcf3490774b7
yangruiboxiu <128752755+yangruiboxiu@users.noreply.github.com>
2023-03-25 16:40:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
+from flask import Flask, jsonify, request
 import torch
+import torchvision.models as models
 import torchvision.transforms as transforms
-from PIL import Image
-import io
-import json
 
-# Define the transformation to be applied to input images
+app = Flask(__name__)
+
+# Load the pre-trained ResNet50 model
+model = models.resnet50(pretrained=True)
+model.eval()
+
+# Define the transform to preprocess images
 transform = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
-    transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225]
-    )
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-# Load the pre-trained ResNet50 model
-model = torch.hub.load('pytorch/vision', 'resnet50', pretrained=True)
-model.eval()
+# Define the API endpoint for predicting image classes
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the image from the request
+    image = request.files['image'].read()
 
-def predict(request):
-    # Parse the request body to extract the image data
-    body = request.get_data()
-    img = Image.open(io.BytesIO(body))
+    # Preprocess the image
+    image_tensor = transform(image)
+    image_tensor = image_tensor.unsqueeze(0)
 
-    # Apply the transformation to the input image
-    img_t = transform(img)
-    img_t = torch.unsqueeze(img_t, 0)
-
-    # Make a prediction with the pre-trained ResNet50 model
+    # Make a prediction
     with torch.no_grad():
-        output = model(img_t)
-        prediction = torch.argmax(output).item()
+        output = model(image_tensor)
+    predictions = torch.nn.functional.softmax(output[0], dim=0)
 
-    # Return the prediction as a JSON response
-    response = {'prediction': prediction}
-    return json.dumps(response)
+    # Return the predictions as JSON
+    return jsonify(predictions.tolist())
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
